f53fde4a84c228aaed4e5b72ca98dca3a7ea33af
njenganelson <nkamau2@gmail.com>
2022-01-21 22:09:06 +0300

    add jquery for form details

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert("JS is reachable")
+//alert("JS is reachable")
+$(document).ready(function () {
+    $('#submit').click(function () {
+        var name = $('#inputName').val()
+        var email = $('#inputEmail').val()
+        var contactMethod = $('[name="gridRadios"]:checked').parent('label').text().trim()
+        var county = $('#inputState').val()
+        var dataChecked = $('[name="dataPrivacy"]:checked').parent('label').text().trim()
+        var contactDate = $('#prefDate').val()
+        var message = $('#comment').val()
+        alert(' Hi ' + name + '\n View entered details: \n Email: ' + email + '\n Preferred Contact Method: '
+            + contactMethod + '\n County of residence: ' + county + '\n Data Privacy: ' + dataChecked + '\n Contact Date: ' + contactDate + '\n' + message +
+            '\n We have received your message. Thank you for reaching out to us.')
+    })
+})
